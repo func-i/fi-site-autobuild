18dbf54244763663e1c04452fd7d689f9d9773e1
tfchang <tingfchang@gmail.com>
2016-03-16 17:54:49 -0400

    refactor Handler, check repo name

--- app/handler.rb
+++ app/handler.rb
@@ -1,9 +1,11 @@
 class Handler
 
   # GitHub account and branches
-  GITHUB_ACC  = "func-i"
-  STAGING     = "staging"
-  PRODUCTION  = "master"
+  GITHUB_ACC    = "func-i"
+  GITHUB_REPO   = "Funci_from_OS"
+  GITHUB_URL    = "https://github.com/#{GITHUB_ACC}/#{GITHUB_REPO}.git"
+  GH_STAGING    = "staging"
+  GH_PRODUCTION = "master"
 
   # Amazon S3 bucket for staging, Heroku env var for the bucket
   S3_STAGING = "fi-website-staging"
@@ -17,16 +19,18 @@ class Handler
   def initialize(data)
     @repo_owner  = data.repository.owner.name
     @repo_name   = data.repository.name;
-    @repo_url    = "https://github.com/#{GITHUB_ACC}/#{@repo_name}.git"
     @repo_branch = data.ref.sub("refs/heads/", "");
   end
 
   def handle
-    # handle only FI's GitHub account: func-i
+    # handle only FI's GitHub account
     return unless @repo_owner === GITHUB_ACC
 
+    # handle only the GitHub repo for FI's website
+    return unless @repo_name === GITHUB_REPO
+
     # handle only push to staging or production branch
-    return unless [STAGING, PRODUCTION].include? @repo_branch
+    return unless [GH_STAGING, GH_PRODUCTION].include? @repo_branch
 
     download
     set_env
@@ -39,7 +43,7 @@ class Handler
   def download
     %x{
       if [ ! -d #{CODE_DIR} ]; then
-        git clone #{@repo_url} #{CODE_DIR}
+        git clone #{GITHUB_URL} #{CODE_DIR}
       fi &&
       cd #{CODE_DIR} &&
       git checkout #{@repo_branch} &&
@@ -49,10 +53,10 @@ class Handler
   end
 
   def set_env
-    if @repo_branch === STAGING
+    if @repo_branch === GH_STAGING
       @jekyll_config = "_config.yml,_config-staging.yml"
       @s3_config = "s3_config_staging"
-    else # @repo_branch === PRODUCTION
+    else # @repo_branch === GH_PRODUCTION
       @jekyll_config = "_config.yml,_config-production.yml"
       @s3_config = "s3_config_production"
     end
