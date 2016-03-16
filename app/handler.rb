6a68feee7d33d5fc118180d6df5218bcf387bc3b
tfchang <tingfchang@gmail.com>
2016-03-16 14:39:17 -0400

    Refactor Handler: use constants

--- app/handler.rb
+++ app/handler.rb
@@ -1,21 +1,26 @@
 class Handler
+
+  GITHUB_ACC  = "func-i"
+  STAGING     = "staging"
+  PRODUCTION  = "master"
+
+  TEMP_DIR = "/tmp"
+  CODE_DIR = "#{TEMP_DIR}/code"
+  SITE_DIR = "#{TEMP_DIR}/site"
+
   def initialize(data)
     @repo_owner  = data.repository.owner.name
     @repo_name   = data.repository.name;
-    @repo_url    = "https://github.com/func-i/#{@repo_name}.git"
+    @repo_url    = "https://github.com/#{GITHUB_ACC}/#{@repo_name}.git"
     @repo_branch = data.ref.sub("refs/heads/", "");
   end
 
   def handle
     # handle only FI's GitHub account: func-i
-    return unless @repo_owner === "func-i"
+    return unless @repo_owner === GITHUB_ACC
 
-    # handle only push to master or staging branch
-    return unless ["master", "staging"].include? @repo_branch
-
-    @temp_dir = "/tmp"
-    @code_dir = "#{@temp_dir}/code"
-    @site_dir = "#{@temp_dir}/site"
+    # handle only push to staging or production branch
+    return unless [STAGING, PRODUCTION].include? @repo_branch
 
     download
     build
@@ -26,10 +31,10 @@ class Handler
 
   def download
     %x{
-      if [ ! -d #{@code_dir} ]; then
-        git clone #{@repo_url} #{@code_dir}
+      if [ ! -d #{CODE_DIR} ]; then
+        git clone #{@repo_url} #{CODE_DIR}
       fi &&
-      cd #{@code_dir} &&
+      cd #{CODE_DIR} &&
       git checkout #{@repo_branch} &&
       git pull origin #{@repo_branch} &&
       cd -
@@ -39,8 +44,8 @@ class Handler
   def build
     config_files = "_config.yml,_config-staging.yml"
     %x{
-      cd #{@code_dir} &&
-      bundle exec jekyll build -d #{@site_dir} --config #{config_files} &&
+      cd #{CODE_DIR} &&
+      bundle exec jekyll build -d #{SITE_DIR} --config #{config_files} &&
       cd -
     }
   end
@@ -48,7 +53,7 @@ class Handler
   def publish
     %x{
       cd #{APP_ROOT.to_path} &&
-      bundle exec s3_website push --site=#{@site_dir}
+      bundle exec s3_website push --site=#{SITE_DIR}
     }
   end
 end
