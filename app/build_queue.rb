5ff6752b8c53c97c98d8851f09ffcc2f44497375
tfchang <tingfchang@gmail.com>
2016-03-30 17:51:42 -0400

    queue auto-build jobs

---
+++ app/build_queue.rb
@@ -0,0 +1,59 @@
+class BuildQueue
+
+  # temp folders on Heroku
+  TEMP_DIR = "/tmp"
+  CODE_DIR = "#{TEMP_DIR}/code"
+  SITE_DIR = "#{TEMP_DIR}/site"
+
+  @queue = :auto_build
+
+  def self.perform(github_url, pusher_email, pusher_name, repo_branch)
+    download(github_url, pusher_email, pusher_name, repo_branch)
+    jekyll_config, s3_config = set_env(repo_branch)
+    build(jekyll_config)
+    publish(s3_config)
+  end
+
+  def self.download(github_url, pusher_email, pusher_name, repo_branch)
+    %x{
+      if [ -d #{CODE_DIR} ]; then
+        cd #{CODE_DIR}
+      else
+        git clone #{github_url} #{CODE_DIR} &&
+        cd #{CODE_DIR} &&
+        git config user.email "#{pusher_email}" &&
+        git config user.name "#{pusher_name}"
+      fi &&
+      git checkout #{repo_branch} &&
+      git pull origin #{repo_branch} &&
+      cd -
+    }
+  end
+
+  def self.set_env(repo_branch)
+    if repo_branch === GH_STAGING
+      jekyll_config = "_config.yml,_config-staging.yml"
+      s3_config = "s3_config_staging"
+    else # repo_branch === GH_PRODUCTION
+      jekyll_config = "_config.yml,_config-production.yml"
+      s3_config = "s3_config_production"
+    end
+
+    return jekyll_config, s3_config
+  end
+
+  def self.build(jekyll_config)
+    %x{
+      cd #{CODE_DIR} &&
+      bundle exec jekyll build -d #{SITE_DIR} --config #{jekyll_config} &&
+      cd -
+    }
+  end
+
+  def self.publish(s3_config)
+    %x{
+      cd #{APP_ROOT.to_path} &&
+      bundle exec s3_website push --site=#{SITE_DIR} --config-dir #{s3_config}
+    }
+  end
+end
